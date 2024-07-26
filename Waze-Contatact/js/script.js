7d1f0d25fd0c6b1798c5404b693827366d596a61
Genivaldo2230 <gda_anjos@hotmail.com.br>
2024-07-26 20:39:16 -0300

    Biuld 1.1.9 Ajustes css

--- Waze-Contatact/js/script.js
+++ Waze-Contatact/js/script.js
@@ -2,3 +2,14 @@ document.querySelector('.footer-section h3').addEventListener('click', function(
     var ul = this.nextElementSibling;
     ul.classList.toggle('visible');
     });
+
+
+    // menu hambug
+    function menuOnClick() {
+        document.getElementById("menu-bar").classList.toggle("change");
+        document.getElementById("nav").classList.toggle("change");
+        document.getElementById("menu-bg").classList.toggle("change-bg");
+      }
+      function menuOnClick() {
+        document.getElementById("menu").classList.toggle("menu-active");
+      }
