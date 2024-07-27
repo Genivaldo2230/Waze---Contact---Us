55d117d8cbab6177c079e675d914b95b696cce01
Genivaldo2230 <gda_anjos@hotmail.com.br>
2024-07-26 22:17:04 -0300

    Biuld 1.2.1

--- Waze-Contatact/js/script.js
+++ Waze-Contatact/js/script.js
@@ -1,15 +1,21 @@
-document.querySelector('.footer-section h3').addEventListener('click', function() {
-    var ul = this.nextElementSibling;
-    ul.classList.toggle('visible');
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  var headers = document.querySelectorAll('.footer-section h3');
+  headers.forEach(function(header) {
+      header.addEventListener('click', function() {
+          var ul = this.nextElementSibling;
+          ul.classList.toggle('visible');
+      });
+  });
+});
+
 
 
     // menu hambug
-    function menuOnClick() {
-        document.getElementById("menu-bar").classList.toggle("change");
-        document.getElementById("nav").classList.toggle("change");
-        document.getElementById("menu-bg").classList.toggle("change-bg");
-      }
+    // function menuOnClick() {
+    //     document.getElementById("menu-bar").classList.toggle("change");
+    //     document.getElementById("nav").classList.toggle("change");
+    //     document.getElementById("menu-bg").classList.toggle("change-bg");
+    //   }
       function menuOnClick() {
         document.getElementById("menu").classList.toggle("menu-active");
       }
